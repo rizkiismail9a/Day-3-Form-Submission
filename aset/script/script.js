a35bf513c36f971349af747366d0f2cc4d33e804
rizkiismail <laksmimentari@gmail.com>
2023-02-22 16:22:58 +0700

    Day-3

---
+++ aset/script/script.js
@@ -0,0 +1,34 @@
+function getData() {
+  let nama = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let nomor = document.getElementById("phone").value;
+  let topik = document.getElementById("subject").value;
+  let pesan = document.getElementById("message").value;
+
+  if (nama == "") {
+    alert("Isi namanya, Boi.");
+  }
+  if (email == "") {
+    alert("Isi alamat emailnya, Boi.");
+  }
+  if (nomor == "") {
+    alert("Isi nomor hp-nya, Boi.");
+  }
+  if (topik == "") {
+    alert("Mau ngomongin apa?");
+  }
+  if (pesan == "") {
+    alert("Mau ngomong apa?");
+  }
+
+  let penerima = "muhamadrizkiismail9a@gmail.com";
+  let a = document.createElement("a");
+  a.href = `mailto:${penerima}?subject=${topik}&body=${pesan}`;
+  a.click();
+
+  console.log(nama);
+  console.log(email);
+  console.log(nomor);
+  console.log(topik);
+  console.log(pesan);
+}
